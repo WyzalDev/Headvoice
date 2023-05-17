75bf01ff600c320d8ea7922a3631d008add0ab63
WyzalDev <wyzaldev@gmail.com>
2023-05-17 21:30:36 +0300

    add character scanEnemies method

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Character : MonoBehaviour
@@ -8,7 +10,6 @@ public class Character : MonoBehaviour
     [Range(0,5)]
     private int armour = 5;
     private int baseDamage = 10;
-    
 
     //In that range character start tracking enemies
     [Header("Enemy Tracking")]
@@ -19,7 +20,13 @@ public class Character : MonoBehaviour
     private Weapon weapon;
 
     private void FixedUpdate() {
-
+        List<Collider2D> objects = scanEnemies();
+        string log = "";
+        foreach (Collider2D item in objects)
+        {
+            log+= item.gameObject.name + "      ";
+        }
+        Debug.Log(log);
     }
 
     public void takeDamage() {
@@ -37,11 +44,15 @@ public class Character : MonoBehaviour
         weapon = newWeapon;
     }
 
-    // private List<Collider2D> scanEnemies() {
-    //     return Physics2D.CircleCastAll().RemoveAll((Collider2D x) => x is Enemy);
-    // }
+    private List<Collider2D> scanEnemies() {
+        List<Collider2D> colliders =new List<Collider2D>(Physics2D.OverlapCircleAll(transform.position, scanRange));
+        colliders.RemoveAll(item => !isHaveEnemyComponent(item));
+        return colliders;
+    }
 
- 
+    private bool isHaveEnemyComponent(Collider2D collider) {
+        return collider.gameObject.GetComponent<AbstractEnemy>() != null;
+    }
 
 
 }
