997d08e654fe9c72fb3eb2ac02e97e18069dd17d
zakhar <nehippi1@yandex.by>
2023-05-17 17:08:08 +0300

    fix enemy class

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,47 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : AbstractEnemy
 {
-    public GameObject[] point = new GameObject[9];
-    public int action, rand = 0;
-    public float speed = 1f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (action == 0 )
-        {
-            if (point[rand].transform.parent !=  null)
-            {
-                for (int i = 0; i<point.Length; i++)
-                {
-                    point[i].transform.parent = null;
-                }
-            }
-                if(transform.position != point[rand].transform.position)
-                {
-                    transform.position = Vector3.MoveTowards(transform.position, point[rand].transform.position, speed * Time.deltaTime);  
-                }
-                else
-                {
-                    rand = Random.Range(0, 9);
-                }
-           
-        }
-    }
-
-    private void onCollisionEnter2D(Collision2D collision)
-    {
-        if (action == 0)
-        {
-            rand = Random.Range(0, 9);
-        }
-    }
-}
+   }
